cc496222999a1a4fc8215225b61a72091ae97a0f
Matt Gunther <mgunther@umn.edu>
2022-10-21 16:28:31 -0400

    asides are padded and colored

--- html_to_pdf.R
+++ html_to_pdf.R
@@ -1,7 +1,7 @@
 source(here::here("r/utilities.r"))
 
 pagedown::chrome_print(
-  here("r_users.Rmd"), 
+  here("r_users.Rmd"),
   here("r_users.pdf"),
   timeout = 600
 )
@@ -10,3 +10,4 @@ pagedown::chrome_print(
   here("test.Rmd"), 
   here("test.pdf")
 )
+
