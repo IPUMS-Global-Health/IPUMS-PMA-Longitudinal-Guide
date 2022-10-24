1185ca10927b57eb66594c042d13ffcface75d60
Matt Gunther <mgunther@umn.edu>
2022-10-24 10:23:40 -0400

    adjust base font size

--- html_to_pdf.R
+++ html_to_pdf.R
@@ -1,10 +1,10 @@
 source(here::here("r/utilities.r"))
 
-pagedown::chrome_print(
-  here("r_users.Rmd"),
-  here("r_users.pdf"),
-  timeout = 600
-)
+# pagedown::chrome_print(
+#   here("r_users.Rmd"),
+#   here("r_users.pdf"),
+#   timeout = 600
+# )
 
 pagedown::chrome_print(
   here("test.Rmd"), 
