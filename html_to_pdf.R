734570fdee8cfd8782ad495ae7fa98572de956eb
Matt Gunther <mgunther@umn.edu>
2022-10-27 10:46:30 -0400

    stata chapter 2

--- html_to_pdf.R
+++ html_to_pdf.R
@@ -1,21 +1,21 @@
 source(here::here("r/utilities.r"))
 
-pagedown::chrome_print(
-  here("r_users.Rmd"),
-  here("r_users.pdf"),
-  timeout = 600
-)
+# pagedown::chrome_print(
+#   here("r_users.Rmd"),
+#   here("r_users.pdf"),
+#   timeout = 600
+# )
 
 pagedown::chrome_print(
   here("test.Rmd"), 
   here("test.pdf")
 )
-# 
-# pagedown::chrome_print(
-#   here("stata_users.Rmd"),
-#   here("stata_users.pdf"),
-#   timeout = 600
-# )
+
+pagedown::chrome_print(
+  here("stata_users.Rmd"),
+  here("stata_users.pdf"),
+  timeout = 600
+)
 
 
 
