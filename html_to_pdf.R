105e0a8d7d60fbf0b4c0c259821b44e8bed6868b
Matt Gunther <mgunther@umn.edu>
2022-11-14 09:32:38 -0500

    update cover pages

--- html_to_pdf.R
+++ html_to_pdf.R
@@ -1,16 +1,14 @@
 source("utilities.r")
 
 pagedown::chrome_print(
-  here("r_users.Rmd"),
-  here("r_users.pdf"),
-  timeout = 600
-)
-
-pagedown::chrome_print(
   here("stata_users.Rmd"),
   here("stata_users.pdf"),
   timeout = 600
 )
 
-
+pagedown::chrome_print(
+  here("r_users.Rmd"),
+  here("r_users.pdf"),
+  timeout = 600
+)
 
